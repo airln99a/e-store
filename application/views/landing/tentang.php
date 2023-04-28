2024fbf1b1044c6459a61f9416dbd9163b7ec146
alfinrarry <alfinrarry@gmail.com>
2023-04-28 23:05:26 +0700

    style page tentang (vision)

--- application/views/landing/tentang.php
+++ application/views/landing/tentang.php
@@ -5,21 +5,20 @@
    </div>
    <div class="row px-xl-5">
      <div class="col-lg-7 mb-5">
-       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14003.098824000543!2d112.72025681002691!3d-7.245208142585376!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7f92e325d1883%3A0x5335302f95938a85!2sJl.%20Sedayu%20No.2%2C%20RT.009%2FRW.03%2C%20Morokrembangan%2C%20Kec.%20Krembangan%2C%20Kota%20SBY%2C%20Jawa%20Timur%2060178!5e0!3m2!1sid!2sid!4v1682189786702!5m2!1sid!2sid" width="800" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3955.704089026098!2d112.69435997465405!3d-7.497885392514446!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7e0bc7156b693%3A0xde1823c2b903ded3!2sJl.%20Raya%20Kludan%20No.231%2C%20Kludan%2C%20Kec.%20Tanggulangin%2C%20Kabupaten%20Sidoarjo%2C%20Jawa%20Timur%2061272!5e0!3m2!1sid!2sid!4v1682697438261!5m2!1sid!2sid" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
      </div>
-     <div class="col-lg-5 mb-5">
-      <h5 class="font-weight-semi-bold mb-3">Vision</h5>
-       <p>To become one of the leading shoe industry companies that can complete both locally and internationally.</p>
-       <br>
-       <br>
-       <br>
-       <br>
-       <br>
-       <h5 class="font-weight-semi-bold mb-3">Mission</h5>
+     <div style = "height:200px" class="d-flex flex-column justify-space-between col-lg-5 mb-5">
+        <div>
+          <h5 class="font-weight-semi-bold mb-3">Vision</h5>
+           <p>To become one of the leading shoe industry companies that can complete both locally and internationally.</p>
+        </div>
+        <div>
+          <h5 class="font-weight-semi-bold mb-3">Mission</h5>
          <p>Prioritizing quality in every stage of production for customer satisfaction.</p>
          <p>Maintain and enchance the company's best image.</p>
          <p>Establish professional partnertships in business.</p>
          <p>Commitment in every current and susstainable service for the sake of improving product quality and excellence.</p>
+       </div>
      </div>
    </div>
  </div>
